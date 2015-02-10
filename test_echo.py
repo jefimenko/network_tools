6f76c22cba19f0515d957c308e335ab79e075b7d
Matthew Lee <ml7@uw.edu>
2015-02-10 00:40:13 -0800

    Added fixtures for test setup and teardown of sockets.

--- test_echo.py
+++ test_echo.py
@@ -4,13 +4,33 @@ import pytest
 
 
 @pytest.fixture(scope='function')
-def setup_client():
-    pass
+def setup_client(request):
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP
+    )
+
+    def closer():
+        client_socket.close()
+    request.addfinalizer(closer)
+
+    return client_socket
 
 
 @pytest.fixture(scope='function')
-def setup_server():
-    pass
+def setup_server(request):
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP
+    )
+
+    def closer():
+        server_socket.close()
+    request.addfinalizer(closer)
+
+    return server_socket
 
 
 # Client tests
