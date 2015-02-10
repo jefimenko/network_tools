0df251a2ed83439d44559a47d1446bf5c611d629
Matthew Lee <ml7@uw.edu>
2015-02-10 00:27:08 -0800

    Added code for creating a server socket and receiving data from a client and responding.

---
+++ echo_server.py
@@ -0,0 +1,14 @@
+import socket
+
+server_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM,
+    socket.IPPROTO_IP)
+
+server_socket.bind(('127.0.0.1', 50000))
+server_socket.listen(1)
+conn, addr = server_socket.accept()
+
+print conn.recv(32)
+conn.sendall('reply')
+conn.close()
