540a8b5e8932f0608349433489e541aff271d697
Matthew Lee <ml7@uw.edu>
2015-02-10 00:35:18 -0800

    Factored out code into functions in client and server scripts.

--- echo_server.py
+++ echo_server.py
@@ -1,14 +1,21 @@
 import socket
 
-server_socket = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
 
-server_socket.bind(('127.0.0.1', 50000))
-server_socket.listen(1)
-conn, addr = server_socket.accept()
+def create_server_socket():
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    return server_socket
 
-print conn.recv(32)
-conn.sendall('reply')
-conn.close()
+
+def comm(server_socket):
+    server_socket.bind(('127.0.0.1', 50000))
+    server_socket.listen(1)
+    return conn, addr = server_socket.accept()
+
+
+def receive(conn):
+    print conn.recv(32)
+    conn.sendall('reply')
+    conn.close()
