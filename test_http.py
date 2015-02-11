b65ddf2cdb917c7338d2db7a53f220487fc79d05
Matthew Lee <ml7@uw.edu>
2015-02-11 12:33:39 -0800

    Added empty test file for http server.

---
+++ test_http.py
@@ -0,0 +1,13 @@
+from echo_server import create_server_socket, response_ok, response_error, parse_request
+
+
+def test_ok():
+    pass
+
+
+def test_error():
+    pass
+
+
+def test_parse():
+    pass
