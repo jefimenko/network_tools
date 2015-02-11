5b404d8ee192872f93f9aacbe61d12ecf5d3dc09
Matthew Lee <ml7@uw.edu>
2015-02-11 14:29:13 -0800

    Added line to ensure byte strings are transmitted.

--- echo_client.py
+++ echo_client.py
@@ -13,7 +13,7 @@ def create_client_socket():
 def comm(client_socket):
     client_socket.connect(('127.0.0.1', 50000))
     # Send argv from command line
-    client_socket.sendall(', '.join(sys.argv[1:]))
+    client_socket.sendall(', '.join(sys.argv[1:]).encode('utf-8'))
     client_socket.shutdown(socket.SHUT_WR)
 
 
