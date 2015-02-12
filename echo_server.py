2245b6a891581dedacbe4507db931ba2274355b2
Matthew Lee <ml7@uw.edu>
2015-02-11 21:32:33 -0800

    Changed multiline strings.

--- echo_server.py
+++ echo_server.py
@@ -6,26 +6,26 @@ def create_server_socket():
         socket.AF_INET,
         socket.SOCK_STREAM,
         socket.IPPROTO_IP)
-    # Set up the socket
+    # Set up the server socket
     server_socket.bind(('127.0.0.1', 50000))
     server_socket.listen(1)
     return server_socket
 
 
 def response_ok():
-    return """
-        HTTP/1.1 200 OK\r\n
-        Content-Type: text/plain\r\n
-        \r\n
-        Hey you.\r\n
-    """
+    return """\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/plain\r\n\r\n\
+Hey you.\r\n\
+"""
 
 
 def response_error(e):
-    return """
-        HTTP/1.1 {error}\r\n
-        \r\n
-    """.format(error=e.args[0])
+    return """\
+HTTP/1.1 {error}\r\n\
+\r\n\
+{cause}\r\n\
+    """.format(error=e.args[0], cause=e.args[1])
 
 
 def parse_request(request_string):
@@ -37,7 +37,13 @@ def parse_request(request_string):
 
 
 def request_protocol_validator(first_line):
-    method, protocol = first_line[0], first_line[2]
+    try:
+        method, protocol = first_line[0], first_line[2]
+    except IndexError:
+        # By returning False, have the server continue to close the current 
+        # connection and wait for the next request without responding.
+        return False
+
     if method != 'GET':
         raise ValueError('403', 'GET method only.')
     if protocol != 'HTTP/1.1':
@@ -53,18 +59,31 @@ if __name__ == '__main__':
     server_socket = create_server_socket()
 
     while True:
+        # Open a connection to a client upon request.
         conn, addr = server_socket.accept()
 
         message = ''
         keep_going = True
+        # Receive a message from a client.
         while keep_going:
             pkt = conn.recv(buffsize)
-            print repr(pkt)
+            # print repr(pkt)
             message = '{}{}'.format(message, pkt)
 
-            # For a last packet of buffsize, this loop iterates one more time
+            # For a last packet of buffsize, this loop iterates one more time.
+            # If the message is over, stop receiving.
             if (len(pkt) < buffsize) or (len(pkt) == 0):
                 keep_going = False
                 print keep_going
-        conn.sendall(message)
+                print repr(message)
+
+        # Parse the message and send the appropriate response.
+        try:
+            # For now, don't do anything with returned URI from message.
+            if parse_request(message):
+                conn.sendall(response_ok())
+        except ValueError as e:
+            conn.sendall(response_error(e))
+
+        # Close the connection after sending a response.
         conn.close()
